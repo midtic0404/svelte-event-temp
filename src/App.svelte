12df2fbe67850c30a6a33af6720a6303670c9deb
Chia Che Chang <midtic0404@gmail.com>
2020-01-17 00:44:26 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Product from './Product.svelte';
+	import Modal from "./Modal.svelte";
+
+	let products = [
+		{
+			id: 'p1',
+			title: 'A Book',
+			price: 9.99
+		}
+	]
+
+	function addToCart(event) {
+		console.log(event);
+		
+	}
+
+	function deleteProduct(event) {
+		console.log(event);
+		
+	}
+</script>
+
+{#each products as product}
+	<Product 
+	{...product}
+	on:add-to-cart={addToCart}
+	on:delete={deleteProduct} />
+{/each}
+
+
+<Modal>
+	<h1>Hi!</h1>
+</Modal>
